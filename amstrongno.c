4f59c7ba00bbc918b2a931f29b0add8a97d83663
Manonmani441 <36624028+Manonmani441@users.noreply.github.com>
2018-06-19 22:16:59 +0530

    Create amstrongno.c

---
+++ amstrongno.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n,sum=0,t,b;
+scanf("%d",&n);
+b=n;
+while(n>0){
+t=n%10;
+sum=sum+(t*t*t);
+n=n/10;
+}
+if(sum==b)
+System.out.println("yes");
+else
+System.out.println("no");
+}
